9e02ccff8cac563c3104e4183359aaa222009f75
tanu8een <tanushreegzb@gmail.com>
2023-06-15 21:59:11 +0530

    added sidebar

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './layout.scss'
+import Sidebar from '../Sidebar/Sidebar'
+
+function Layout() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Layout
